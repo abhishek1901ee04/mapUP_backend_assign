ca0f1ebfa9428b1f9223e1556f064288fb05be56
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2023-12-17 17:10:34 +0530

    updating port code

--- file.go
+++ file.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	// "fmt"
 	"net/http"
+	"os"
 	"sort"
 	"sync"
 	"time"
@@ -23,73 +23,84 @@ func main() {
 	http.HandleFunc("/process-single", processSingle)
 	http.HandleFunc("/process-concurrent", processConcurrent)
 
+	port := getPort()
+
 	// Starting the server
-	log.Println("Server listening on :8000...")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	log.Printf("Server listening on :%s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func processSingle(w http.ResponseWriter, r *http.Request){
-	handleRequest(w,r,false)
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		// Default to 8000 if PORT environment variable is not set
+		port = "8000"
+	}
+	return port
 }
 
-func processConcurrent(w http.ResponseWriter , r *http.Request){
-	handleRequest(w,r,true)
+func processSingle(w http.ResponseWriter, r *http.Request) {
+	handleRequest(w, r, false)
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request,concurrent bool){
+func processConcurrent(w http.ResponseWriter, r *http.Request) {
+	handleRequest(w, r, true)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request, concurrent bool) {
 	var req InputRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request format",http.StatusBadRequest)
+		http.Error(w, "Invalid request format", http.StatusBadRequest)
 		return
 	}
 
 	startTime := time.Now()
 
 	var sortedArrays [][]int
-	
+
 	if concurrent {
 		sortedArrays = sortConcurrent(req.ToSort)
-	}else{
+	} else {
 		sortedArrays = sortSequential(req.ToSort)
 	}
 	timeTaken := time.Since(startTime)
 
-	response := OutputResponse {
+	response := OutputResponse{
 		SortedArrays: sortedArrays,
-		TimeNs: timeTaken.Nanoseconds(),
+		TimeNs:       timeTaken.Nanoseconds(),
 	}
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-func sortSequential(arrays [][]int) [][]int{
-	sortedArrays := make([][]int,len(arrays))
+func sortSequential(arrays [][]int) [][]int {
+	sortedArrays := make([][]int, len(arrays))
 
-	for i,arr := range arrays {
+	for i, arr := range arrays {
 		sortedArr := make([]int, len(arr))
-		copy(sortedArr,arr)
+		copy(sortedArr, arr)
 		sort.Ints(sortedArr)
 		sortedArrays[i] = sortedArr
 	}
 	return sortedArrays
 }
 
-func sortConcurrent(arrays [][]int) [][]int{
+func sortConcurrent(arrays [][]int) [][]int {
 	var wg sync.WaitGroup
 	wg.Add(len(arrays))
 
 	sortedArrays := make([][]int, len(arrays))
 
-	for i ,arr := range arrays{
-		go func( i int, arr[]int){
+	for i, arr := range arrays {
+		go func(i int, arr []int) {
 			defer wg.Done()
-			sortedArr := make([]int,len(arr))
-			copy(sortedArr,arr)
+			sortedArr := make([]int, len(arr))
+			copy(sortedArr, arr)
 			sort.Ints(sortedArr)
 			sortedArrays[i] = sortedArr
-		}(i,arr)
+		}(i, arr)
 	}
 	wg.Wait()
 
